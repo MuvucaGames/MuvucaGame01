d3e4188966bacd4cd0002a7ecd21f277ba500ba5
StefanW <scaw.dev@gmail.com>
2015-07-24 10:34:39 +0200

    Add SplashScreen and MainMenu scenes

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(AudioSource))]
+
+public class Intro : MonoBehaviour {
+
+	public MovieTexture movie;
+	private AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<RawImage> ().texture = movie as MovieTexture;
+		audio = GetComponent<AudioSource> ();
+		audio.clip = movie.audioClip;
+		movie.Play ();
+		audio.Play ();
+	}
+
+	void Update(){
+		if (!movie.isPlaying) {
+			Application.LoadLevel("MainMenu");
+		}
+	}
+}
