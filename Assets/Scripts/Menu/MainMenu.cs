98d3757139be66598e056b68977412a64248d267
inb4iba <iba1987@gmail.com>
2015-07-30 20:38:51 -0300

    Main Menu
    Options scene: only with buttons to call empty panels;
    SelectSlot scene: simulates saved game with placeholder variables;
    MainMenu scene: continue button is shown based on the checkbox "Exist
    Saved Game"

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	//Checkbox on inspector to test if there is a saved game
+	public bool existSavedGame;
+
+	// Use this for initialization
+	void Start () {
+		if (!existSavedGame) {
+			GameObject.Find("ContinueButton").SetActive(false);
+		}
+	}
+
+	public void ContinueButton(){
+		//Application.LoadLevel("Test");
+		Debug.Log("Continue");
+	}
+
+	public void NewGameButton(){
+		Application.LoadLevel("SelectSlot");
+		Debug.Log("New Game");
+	}
+
+	public void LoadGameButton(){
+		Application.LoadLevel("SelectSlot");
+		Debug.Log("Load Game");
+	}
+
+	public void OptionsButton(){
+		Application.LoadLevel("Options");
+		Debug.Log("Options");
+	}
+
+	public void CreditsButton(){
+		Debug.Log("Credits");
+		Application.LoadLevel("Credits");
+	}
+
+	public void ExitGameButton(){
+		Debug.Log("Exit Game");
+		Application.Quit ();
+	}
+}
---
+++ Assets/Scripts/Menu/Options.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Options : MonoBehaviour {
+
+	public GameObject graphicsPanel, controlsPanel, soundPanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void GraphicsButton (){
+		soundPanel.SetActive (false);
+		controlsPanel.SetActive (false);
+		graphicsPanel.SetActive (true);
+		Debug.Log ("Graphics");
+	}
+
+	public void SoundButton (){
+		soundPanel.SetActive (true);
+		controlsPanel.SetActive (false);
+		graphicsPanel.SetActive (false);
+		Debug.Log ("Sound");
+	}
+
+	public void ControlsButton (){
+		soundPanel.SetActive (false);
+		controlsPanel.SetActive (true);
+		graphicsPanel.SetActive (false);
+		Debug.Log ("Controls");
+	}
+
+	public void RestoreButton (){
+		Debug.Log ("Restore to Default");
+	}
+	
+	public void BackButton(){
+		Application.LoadLevel ("MainMenu");
+	}
+}
