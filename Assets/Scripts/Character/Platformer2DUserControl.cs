461a0e889fb9113f0b47ff155d2759177c398643
Rafael Vargas <rsvargas@gmail.com>
2015-07-30 23:46:59 -0300

    Gettign rid of the last of those unused standard assets.

--- Assets/Scripts/Character/Platformer2DUserControl.cs
+++ Assets/Scripts/Character/Platformer2DUserControl.cs
@@ -1,6 +1,5 @@
 using System;
 using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
 
 [RequireComponent(typeof (PlatformerCharacter2D))]
 public class Platformer2DUserControl : MonoBehaviour
@@ -20,7 +19,7 @@ public class Platformer2DUserControl : MonoBehaviour
         if (!m_Jump)
         {
             // Read the jump input in Update so button presses aren't missed.
-            m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            m_Jump = Input.GetButtonDown("Jump");
         }
     }
 
@@ -28,8 +27,8 @@ public class Platformer2DUserControl : MonoBehaviour
     private void FixedUpdate()
     {
         // Read the inputs.
-        bool crouch = Input.GetKey(KeyCode.LeftControl);
-        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        bool crouch = Input.GetAxis("Vertical") < 0f;
+        float h = Input.GetAxis("Horizontal");
         // Pass all parameters to the character control script.
         m_Character.Move(h, crouch, m_Jump);
         m_Jump = false;
--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -11,9 +11,6 @@ public class Intro : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        //temporary as I cant import the movie
-        StartCoroutine(LoadMainAfterTime(5));
-
         GetComponent<RawImage>().texture = movie as MovieTexture;
 		audio = GetComponent<AudioSource> ();
 		audio.clip = movie.audioClip;
@@ -26,12 +23,4 @@ public class Intro : MonoBehaviour {
 			Application.LoadLevel("MainMenu");
 		}
 	}
-
-    //temporary as I cant import the movie
-    IEnumerator LoadMainAfterTime(int secs)
-    {
-        yield return new WaitForSeconds(secs);
-        Application.LoadLevel("MainMenu");
-        yield break;
-    }
 }
