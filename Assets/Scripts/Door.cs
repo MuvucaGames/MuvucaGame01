9a654b7d4d90f62c30a891a1aa057c01e6b8b106
Pablo Saraiva <pablosaraiva@gmail.com>
2015-08-05 10:09:48 -0300

    Added WeightButton and ActionableDoor

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour, IActionableElement {
+	private Vector3 closedPosition;
+	private Vector3 openedPosition;
+	void Awake()
+	{
+		closedPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+		openedPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 4, gameObject.transform.position.z);
+	}
+
+	public void Activate()
+	{
+		gameObject.transform.position = openedPosition;
+	}
+
+	public void Deactivate()
+	{
+		gameObject.transform.position = closedPosition;
+	}
+
+}
