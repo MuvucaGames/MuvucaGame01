bed6e1d3074fc8fc75a0065585dc2676db54efb3
Pablo Saraiva <pablosaraiva@gmail.com>
2015-08-04 22:32:46 -0300

    Added Checkpoints and CheckpointManager

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -2,14 +2,12 @@
 using System.Collections;
 
 public class Checkpoint : MonoBehaviour {
+	public Vector3 heroStrongPosition;
+	public GameObject manager;
+	public int checkpointOrder;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnTriggerEnter2D() {
+		CheckpointManager script = manager.GetComponent<CheckpointManager> ();
+		script.RegisterCheckpoint (this);
 	}
 }
---
+++ Assets/Scripts/CheckpointManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CheckpointManager : MonoBehaviour {
+	public GameObject heroStrong;
+	private static int lastCheckpoint = 0;
+
+	void Awake()
+	{
+		if (lastCheckpoint > 0) 
+		{
+			GameObject[] checkpoints = GameObject.FindGameObjectsWithTag("Checkpoint");
+			foreach (GameObject o in checkpoints)
+			{
+				Checkpoint script = o.GetComponent<Checkpoint>();
+				if (script.checkpointOrder == lastCheckpoint)
+				{
+					heroStrong.transform.position = script.heroStrongPosition;
+					break;
+				}
+			}
+		}
+	}
+
+	public void RegisterCheckpoint(Checkpoint checkpoint)
+	{
+		if (checkpoint.checkpointOrder > lastCheckpoint) 
+		{
+			lastCheckpoint = checkpoint.checkpointOrder;
+		}
+	}
+
+}
