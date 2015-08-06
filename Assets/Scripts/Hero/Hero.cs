e9e4b6c50f78a3764d83c602d1b8cb8e2b81e947
inb4iba <iba1987@gmail.com>
2015-08-06 00:40:09 -0300

    CameraController
    Modified some things in hero class, making possible switch between
    characters by pressing 'Tab'.
    
    Is possible to check the camera function on the scene
    "TestSceneThornAndCheckpoints"

--- Assets/Scripts/Hero/Hero.cs
+++ Assets/Scripts/Hero/Hero.cs
@@ -13,6 +13,15 @@ public abstract class Hero : MonoBehaviour {
 	[SerializeField] private Collider2D headCollider;
 	[SerializeField] private Collider2D heroPlatform;
 
+	protected bool m_isActive = false;
+	public bool IsActive{
+		get { return m_isActive; }
+		protected set { m_isActive = value; }
+	}
+	private bool m_onAir = false;
+	public bool OnAir{
+		get { return m_onAir; }
+	}
 
 	private bool m_FacingRight = true;
 	private Rigidbody2D rigidBody2D;
@@ -20,7 +29,7 @@ public abstract class Hero : MonoBehaviour {
 	private double jumpForce;
 	private Animator animator;
 
-	void Awake(){
+	protected virtual void Awake(){
 		rigidBody2D = GetComponent<Rigidbody2D> ();
 
 		//get the animator
@@ -35,44 +44,49 @@ public abstract class Hero : MonoBehaviour {
 	}
 
 	public void Move(float horizontalMove, bool crouch, bool jump){
+
 		//WALK HORIZONTALY
-		rigidBody2D.AddForce(new Vector2(horizontalMove*WalkForce,0), ForceMode2D.Impulse);
-		
+		rigidBody2D.AddForce (new Vector2 (horizontalMove * WalkForce, 0), ForceMode2D.Impulse);
+	
 		//LIMIT WWALKING SPEED
 		if (Mathf.Abs (rigidBody2D.velocity.x) > MaxWalkingSpeed) {
-			rigidBody2D.velocity = new Vector2(Mathf.Sign(rigidBody2D.velocity.x) * MaxWalkingSpeed , rigidBody2D.velocity.y);
+			rigidBody2D.velocity = new Vector2 (Mathf.Sign (rigidBody2D.velocity.x) * MaxWalkingSpeed, rigidBody2D.velocity.y);
 		}
 
 		//SET WALKING ANIMATION
-		animator.SetBool("walk", Mathf.Abs(horizontalMove)>0);
+		animator.SetBool ("walk", Mathf.Abs (horizontalMove) > 0);
 
 		//CROUCH
 		if (crouch && !headCollider.isTrigger) {
 			//Transfor head in trigger
 			headCollider.isTrigger = crouch;
-
-			heroPlatform.offset = heroPlatform.offset - new Vector2(0f, headCollider.bounds.extents.y*2);
+				heroPlatform.offset = heroPlatform.offset - new Vector2 (0f, headCollider.bounds.extents.y * 2);
 			//Crouch Animation
-			animator.SetBool("crouch", true);
-		} else if(!crouch && headCollider.isTrigger && !Physics2D.OverlapArea(headCollider.bounds.min, headCollider.bounds.max, whatIsGround.value)){
+			animator.SetBool ("crouch", true);
+		} else if (!crouch && headCollider.isTrigger && !Physics2D.OverlapArea (headCollider.bounds.min, headCollider.bounds.max, whatIsGround.value)) {
 			headCollider.isTrigger = crouch;
 			heroPlatform.offset = headCollider.offset;
 			//Crouch Animatio
-			animator.SetBool("crouch", false);
+			animator.SetBool ("crouch", false);
 		}
 
 		//JUMP, IF GROUDED OR ON OTHER HERO PLATFORM
-		if(jump && Physics2D.OverlapCircle(transform.position, 0.2f, whatIsGround.value | heroPlatformMask.value | mapInteractiveObjectsMask.value)){
+		if (jump && Physics2D.OverlapCircle (transform.position, 0.2f, whatIsGround.value | heroPlatformMask.value | mapInteractiveObjectsMask.value)) {
 			//Impulse to Jump that height
 			//more info look at http://hyperphysics.phy-astr.gsu.edu/hbase/impulse.html and reverse http://hyperphysics.phy-astr.gsu.edu/hbase/flobj.html#c2
-			rigidBody2D.AddForce(new Vector2(0f, (float)jumpForce), ForceMode2D.Impulse);
+			rigidBody2D.AddForce (new Vector2 (0f, (float)jumpForce), ForceMode2D.Impulse);
 		}
 
-		if((horizontalMove>0 && !m_FacingRight) || (horizontalMove<0 && m_FacingRight)){
+		if (Physics2D.OverlapCircle (transform.position, 0.2f, whatIsGround.value | heroPlatformMask.value | mapInteractiveObjectsMask.value))
+			m_onAir = false;
+		else
+			m_onAir = true;
+
+		if ((horizontalMove > 0 && !m_FacingRight) || (horizontalMove < 0 && m_FacingRight)) {
 			//Flip the animation
 			// Switch the way the player is labelled as facing.
 			m_FacingRight = !m_FacingRight;
-			
+		
 			// Multiply the player's x local scale by -1.
 			Vector3 theScale = transform.localScale;
 			theScale.x *= -1;
@@ -81,4 +95,7 @@ public abstract class Hero : MonoBehaviour {
 
 	}
 
+	public void ChangeHero(){
+		m_isActive = !m_isActive;
+	}
 }
