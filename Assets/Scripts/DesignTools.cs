e8f22454ea7a6a84037b770e8d470e8386e81328
StefanW <scaw.dev@gmail.com>
2015-08-07 13:10:04 +0200

    Fixes

--- Assets/Scripts/DesignTools.cs
+++ Assets/Scripts/DesignTools.cs
@@ -5,6 +5,7 @@ public class DesignTools : MonoBehaviour {
 
 	public Hero fastHero;
 	public Hero strongHero;
+	public CheckpointManager cm;
 
 	// Use this for initialization
 	void Start () {
@@ -19,5 +20,11 @@ public class DesignTools : MonoBehaviour {
 	public void TooglePanel(){
 		GameObject panel = transform.FindChild ("Panel").gameObject;
 		panel.SetActive (!panel.activeSelf);
+	}
+
+	public void ResetLevel(){
+		//TODO fix this mess
+		cm.ResetCheckpoins ();
+		Application.LoadLevel(Application.loadedLevel);
 	}
 }
