abb6af63ee38d5e8dae5fd868a9c7f593a603f46
Marcius Oliveira <marcius.programador@gmail.com>
2015-08-10 21:57:28 -0300

    Criando helper para o editor
    Agora o criador da faser vai conseguir visualizar para onde os botões
    estão apontando.

---
+++ Assets/Editor/HelperActionableObjects.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(WeigthButton))]
+public class HelperActionableObjects : Editor {
+
+    public void OnSceneGUI()
+    {
+        WeigthButton myTarget = (WeigthButton)target;
+        foreach (GameObject actionableObject in myTarget.actionableObjects)
+        {
+
+            Handles.color = Color.green;
+            Handles.DrawLine(myTarget.transform.position, actionableObject.transform.position);
+        }
+        
+    }
+}
