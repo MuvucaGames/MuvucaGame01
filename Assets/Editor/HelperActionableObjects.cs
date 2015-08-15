e4cbca7d850f054fa89d29a1f977c055bbd9a03a
StefanW <scaw.dev@gmail.com>
2015-08-16 00:45:08 +0200

    ActionableElement Class + Button Material + Fix Mass
    Unity can not serialize Interface, so we changed the IActionableElement
    to a abstract class that inherits from MonoBehavior. This way we can
    have ActionableElement on the Inspector, and we are sure that the
    LevelDesigner can only link a Actionable to the buttons

--- Assets/Editor/HelperActionableObjects.cs
+++ Assets/Editor/HelperActionableObjects.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using UnityEngine;
 using UnityEditor;
 using System;
@@ -9,25 +9,17 @@ public class HelperActionableObjects : Editor {
     public void OnSceneGUI()
     {
         WeigthButton myTarget = (WeigthButton)target;
-        foreach (GameObject actionableObject in myTarget.actionableObjects)
+        foreach (ActionableElement actionableElement in myTarget.actionableObjects)
         {
-            if (actionableObject == null)
+            if (actionableElement == null)
             {
                 string message = String.Format("Yo mama told you to link that door to that button. Never forget again, son", myTarget.name );
                 Debug.LogError(message);
                 Debug.Break();
                 return;
             }
-            IActionableElement actionableElement = actionableObject.GetComponent<IActionableElement>();
-            if (actionableElement == null)
-            {
-                string message = String.Format("Yo mama told you the {0} is a invalid 'door'. Never forget again, son.", actionableObject.name);
-                Debug.LogError(message);
-                Debug.Break();
-                return;
-            } 
             Handles.color = Color.green;
-            Handles.DrawLine(myTarget.transform.position, actionableObject.transform.position);
+            Handles.DrawLine(myTarget.transform.position, actionableElement.transform.position);
         }
         
     }
--- Assets/Scenes/TestScenes/MarciusTest/TestScripts/ChangeColorElement.cs
+++ Assets/Scenes/TestScenes/MarciusTest/TestScripts/ChangeColorElement.cs
@@ -1,25 +1,17 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class ChangeColorElement : MonoBehaviour, IActionableElement {
+public class ChangeColorElement : ActionableElement {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+    
+	public override void Activate ()
+	{
+		gameObject.GetComponent<SpriteRenderer>().color = Color.green;
 	}
 
-    public void Activate()
-    {
-        gameObject.GetComponent<SpriteRenderer>().color = Color.green;
-    }
-
-    public void Deactivate()
-    {
-        gameObject.GetComponent<SpriteRenderer>().color = Color.red;
-    }
+	public override void Deactivate ()
+	{
+		gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+	}
+    
 }
