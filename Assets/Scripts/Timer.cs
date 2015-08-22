44ff44398d63515c3da599a1366ad4a27878d344
Pablo Saraiva <pablosaraiva@gmail.com>
2015-08-22 11:58:32 -0300

    Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : ActionableElement {
+
+	private float timeLeftInSeconds;
+	private bool isActive;
+
+	public int initialTimeInSeconds;
+	public ActionableElement actionableElement;
+
+	public override void Activate()
+	{
+		isActive = true;
+	}
+
+	public override void Deactivate()
+	{
+		isActive = false;
+	}
+
+	void Start () {
+		timeLeftInSeconds = initialTimeInSeconds;
+	}
+	
+	void Update () {
+		if (isActive) {
+			timeLeftInSeconds -= Time.deltaTime;
+		}
+
+	
+		if (timeLeftInSeconds <= 0) {
+			timeout ();
+		} 
+	}
+
+	void timeout() {
+		actionableElement.Activate ();
+	}
+
+	void resetTimer() {
+		actionableElement.Deactivate ();
+	}
+}
