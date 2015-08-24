c7a6c99c5c8910e48a79910e159a4f13cd186cdd
Pablo Saraiva <pablosaraiva@gmail.com>
2015-08-24 10:33:22 -0300

    Dialog System - First steps

---
+++ Assets/Scripts/Elements/Dialog/Balloon.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Balloon : MonoBehaviour {
+
+	public Transform speaker;
+	public Vector2 speakerOffset;
+	public int timeToLiveInSeconds;
+	private float timeLeft;
+
+	void Update () {
+		Vector3 position = new Vector3(speaker.position.x + speakerOffset.x, speaker.position.y + speakerOffset.y, 0);
+		gameObject.transform.position = position;
+	}
+}
