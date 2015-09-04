5e1ff0ee44645336d12c8e4f29738be07652b68d
Rafael Correia <rafaeljpc@gmail.com>
2015-09-04 01:23:20 -0300

    Refactoring of Controller and fixing some wrong animation states

--- Assets/Scripts/Hero/HeroControl.cs
+++ Assets/Scripts/Hero/HeroControl.cs
@@ -5,17 +5,65 @@ public class HeroControl : MonoBehaviour {
 
 	private Hero hero;
 
-	private bool m_Jump;
+	// Input States
+	private float _walkButtonPressed = 0.0f;
+	private bool _jumpButtonPressed = false;
+	private bool _crouchButtonPressed = false;
+	private bool _pushButtonPressed = false;
+	private bool _actionButtonPressed = false;
+	private bool _changeHeroButtonPressed = false;
+
 
 	void Awake () {
 		hero = GetComponent<Hero> ();
 	}
 
-	void Update(){
+	private void detectButtonStates(){
+		// Walk
+		if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.0f)
+			_walkButtonPressed = Input.GetAxis("Horizontal");
+
+		// Change Hero
+		if (Input.GetButtonDown("ChangeHero"))
+			_changeHeroButtonPressed = true;
+		if (Input.GetButtonUp("ChangeHero"))
+			_changeHeroButtonPressed = false;
+
+		// Jump
+		if (Input.GetButtonDown("Jump"))
+			_jumpButtonPressed= true;
+		if (Input.GetButtonUp("Jump"))
+			_jumpButtonPressed = false;
+
+		// Crouch
+		if (Input.GetButtonDown("Crouch"))
+			_crouchButtonPressed= true;
+		if (Input.GetButtonUp("Crouch"))
+			_crouchButtonPressed = false;
+
+		// Push
+		if (Input.GetButtonDown("Push"))
+			_pushButtonPressed= true;
+		if (Input.GetButtonUp("Push"))
+			_pushButtonPressed = false;
+
+		// Action
+		if (Input.GetButtonDown("Action"))
+			_actionButtonPressed= true;
+		if (Input.GetButtonUp("Action"))
+			_actionButtonPressed = false;
+	}
+
+	void Update() {
+		detectButtonStates();
+	}
+
+	/*void Update(){
 		if (!m_Jump)
 		{
 			// Read the jump input in Update so button presses aren't missed.
-			m_Jump = Input.GetKeyDown(KeyCode.Space);
+			m_Jump = Input.GetButtonDown("Jump");
+			Debug.Log("Jump=" + m_Jump);
 		}
 
         if (Input.GetKeyDown(KeyCode.Tab))
@@ -28,15 +76,25 @@ public class HeroControl : MonoBehaviour {
         {
             hero.SendMessage("DoAction");
         }
-	}
+	}*/
 	
 	void FixedUpdate () {
 
+		if (_changeHeroButtonPressed) {
+			hero.ChangeHero();
+			SoundManager.Instance.SendMessage("PlaySFXSwap");
+		}
+
+		if (_actionButtonPressed) {
+			hero.SendMessage("DoAction");
+		}
+
+
 		if (hero.IsActive)
-			hero.Move (Input.GetAxis ("Horizontal"), Input.GetKey (KeyCode.DownArrow) | Input.GetKey (KeyCode.S), m_Jump);
+			hero.Move(_walkButtonPressed, _crouchButtonPressed, _jumpButtonPressed);
 		else
 			hero.StopWalk ();
 
-		m_Jump = false;
+		_jumpButtonPressed = false;
 	}
 }
