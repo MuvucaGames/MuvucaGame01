35900cb6cd628716e5f99778b8a55de5f7e40664
StefanW <scaw.dev@gmail.com>
2015-10-19 05:42:13 +0200

    Travelator Scripts

---
+++ Assets/Editor/TravelatorEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor (typeof (Travelator))]
+public class TravelatorEditor : Editor {
+
+
+	public override void OnInspectorGUI ()
+	{
+		base.OnInspectorGUI ();
+		Travelator travelator = (Travelator)target;
+
+		Transform pointA = travelator.transform.FindChild("PointA");
+	}
+
+	public void OnSceneGUI(){
+		Travelator travelator = (Travelator)target;
+		
+		Transform pointA = travelator.transform.FindChild("PointA");
+		Transform pointB = travelator.transform.FindChild("PointB");
+		
+		Debug.Log (pointA.position);
+		Handles.color = Color.blue;
+		Handles.DrawLine (pointA.position, pointB.position);
+	}
+
+}
---
+++ Assets/Scripts/Elements/Travelator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Travelator : MonoBehaviour {
+
+	[SerializeField]private bool goLeft = true;
+
+
+
+}
