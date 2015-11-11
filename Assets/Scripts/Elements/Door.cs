e6e73cbddd16eb7255c116c3d9f5615031cc4cd7
guilhermercaetano <eeeee.guilherme@gmail.com>
2015-11-11 00:54:00 -0200

    Some working done with warp door
    I still need to fix some problems such:
    - Travel velocity
    - Landing on ground instead of air

--- Assets/Scripts/Elements/Door.cs
+++ Assets/Scripts/Elements/Door.cs
@@ -4,7 +4,10 @@ using System.Collections;
 public class Door : ActionableElement {
 	private Vector3 closedPosition;
 	private Vector3 openedPosition;
-	void Awake()
+    // Check door closed or opened: if checked, then its opened, if unchecked, then closed
+    public bool isDoorOpened;
+
+    void Awake()
 	{
 		closedPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
 		openedPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 4, gameObject.transform.position.z);
@@ -15,12 +18,13 @@ public class Door : ActionableElement {
 		gameObject.transform.position = openedPosition;
 		gameObject.GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezeAll;
         SoundManager.Instance.SendMessage("PlaySFXOpenGate");
+        isDoorOpened = true;
 	}
 
 	public override void Deactivate()
 	{
 		gameObject.GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX;
         SoundManager.Instance.SendMessage("PlaySFXCloseGate");
+        isDoorOpened = false;
 	}
-
 }
