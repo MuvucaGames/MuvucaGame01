7cb9e8f0ee7e3da31cf3be8c4bc01368b874c911
guilhermercaetano <eeeee.guilherme@gmail.com>
2015-11-15 18:45:12 -0200

    Revert "Revert "Update - Trigger Action for Warp Door fixed""
    This reverts commit 9db8fd52486bc2b37b507c60e853ea69d1928564.

--- Assets/Scripts/Elements/Door.cs
+++ Assets/Scripts/Elements/Door.cs
@@ -1,83 +1,75 @@
 using UnityEngine;
 using System.Collections;
 
-public class Door : ActionableElement {
-<<<<<<< HEAD
-	private Vector3 closedPosition;
-	private Vector3 openedPosition;
-    // Check door closed or opened: if checked, then its opened, if unchecked, then closed
-    public bool isDoorOpened;
+public class Door : ActionableElement
+{
+    public Vector3 openedPosition = new Vector3(0, 3, 0);
+    public float maxSpeed = 0.5f;
+    public float maxForce = 10f;
+    public bool fallWithGravity = true;
 
+    private Rigidbody2D movablePart;
+    Coroutine movement;
     void Awake()
-=======
-	public Vector3 openedPosition = new Vector3(0,3,0);
-	public float maxSpeed = 0.5f;
-	public float maxForce = 10f;
-	public bool fallWithGravity = true;
+    {
+        movablePart = GetComponentInChildren<Rigidbody2D>();
+    }
 
-	private Rigidbody2D movablePart;
-	Coroutine movement;
-	void Awake()
->>>>>>> origin/master
-	{
-		movablePart = GetComponentInChildren<Rigidbody2D>();
-	}
-
-	public override void Activate()
-	{
-		if (movement != null)
-			StopCoroutine (movement);
-		movement = StartCoroutine (Movement(transform.TransformPoint (openedPosition)));
+    public override void Activate()
+    {
+        if (movement != null)
+            StopCoroutine(movement);
+        movement = StartCoroutine(Movement(transform.TransformPoint(openedPosition)));
         SoundManager.Instance.SendMessage("PlaySFXOpenGate");
-        isDoorOpened = true;
-	}
+    }
 
-	public override void Deactivate()
-	{
-<<<<<<< HEAD
-		gameObject.GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX;
+    public override void Deactivate()
+    {
+
+        if (movement != null)
+            StopCoroutine(movement);
+
+        if (fallWithGravity)
+        {
+            movablePart.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX;
+        }
+        else
+        {
+            movement = StartCoroutine(Movement(transform.position));
+        }
+
         SoundManager.Instance.SendMessage("PlaySFXCloseGate");
-        isDoorOpened = false;
-=======
+    }
 
-		if (movement != null)
-			StopCoroutine (movement);
+    private IEnumerator Movement(Vector3 target)
+    {
+        movablePart.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX;
 
-		if (fallWithGravity) {
-			movablePart.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX;
-		} else {
-			movement = StartCoroutine(Movement(transform.position));
-		}
+        float distanceToTarget = Vector3.Distance(movablePart.transform.position, target);
 
-		SoundManager.Instance.SendMessage("PlaySFXCloseGate");
-	}
+        while (distanceToTarget > 0.05f)
+        {
+            //recalculate the distance between the element's position to the target's position
+            distanceToTarget = Vector3.Distance(movablePart.transform.position, target);
 
-	private IEnumerator Movement(Vector3 target){
-		movablePart.constraints = RigidbodyConstraints2D.FreezeRotation| RigidbodyConstraints2D.FreezePositionX;
+            //Create a vector that point to target
+            Vector3 vel = (target - movablePart.transform.position);
+            //Normalize the vector
+            vel.Normalize();
 
-		float distanceToTarget = Vector3.Distance (movablePart.transform.position, target);
+            movablePart.AddForce(new Vector2(vel.x, vel.y) * maxForce);
 
-		while (distanceToTarget>0.05f) {
-			//recalculate the distance between the element's position to the target's position
-			distanceToTarget = Vector3.Distance (movablePart.transform.position, target);
-			
-			//Create a vector that point to target
-			Vector3 vel = (target - movablePart.transform.position);
-			//Normalize the vector
-			vel.Normalize ();
+            if (movablePart.velocity.magnitude > maxSpeed)
+            {
+                movablePart.velocity = Vector2.ClampMagnitude(movablePart.velocity, maxSpeed);
+            }
 
-			movablePart.AddForce(new Vector2(vel.x,vel.y) * maxForce);
+            yield return null;
+        }
 
-			if(movablePart.velocity.magnitude > maxSpeed){
-				movablePart.velocity = Vector2.ClampMagnitude( movablePart.velocity, maxSpeed);
-			}
-
-			yield return null;
-		}
-
-		movablePart.constraints = RigidbodyConstraints2D.FreezeAll;
+        movablePart.constraints = RigidbodyConstraints2D.FreezeAll;
 
 
->>>>>>> origin/master
-	}
+    }
+
 }
