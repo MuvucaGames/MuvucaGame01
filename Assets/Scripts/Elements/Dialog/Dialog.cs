a435682103dd017fdb1fd46a37bab365a2c89224
StefanW <scaw.dev@gmail.com>
2015-11-27 00:14:08 +0100

    Test

---
+++ Assets/Scripts/Elements/Dialog/Dialog.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Dialog : ScriptableObject{
+
+	public List<Sentence> senteces;
+}
---
+++ Assets/Scripts/Elements/Dialog/Sentence.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Sentence : ScriptableObject
+{
+	public List<Sprite> images;
+
+	public float timeInSeconds;
+}
+
