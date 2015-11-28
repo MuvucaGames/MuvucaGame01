03ec19b1ca1a72e696f53994927a9c9b833fe648
Pablo Saraiva <pablosaraiva@gmail.com>
2015-11-28 04:17:30 -0200

    Triggers Only When Both Players are Inside

--- Assets/Scripts/Elements/TriggerForActionableElement.cs
+++ Assets/Scripts/Elements/TriggerForActionableElement.cs
@@ -4,18 +4,47 @@ using System.Collections;
 public class TriggerForActionableElement : Activator {
 
 	public bool triggerOnlyOnce = false;
+    public bool triggerOnlyWhenBothCharactersAreInside = false;
 	private bool alreadyTrigged = false;
+    private int playerCount = 0;
 
 	void OnTriggerEnter2D(Collider2D other) {
-		if (triggerOnlyOnce && alreadyTrigged) 
+        if (other.tag == "Player")
+        {
+            playerCount++;
+            print(playerCount);
+        }
+
+        if (triggerOnlyOnce && alreadyTrigged) 
 		{
 			return;
 		}
 
-		if (other.tag == "Player") 
-		{
-			ActivateAll();
-			alreadyTrigged = true;
-		}
-	}
+        if (triggerOnlyWhenBothCharactersAreInside)
+        {
+            if (playerCount == 2)
+            {
+                ActivateAll();
+                alreadyTrigged = true;
+            } else
+            {
+                // wait for another player
+            }
+        }
+        else
+        {
+            ActivateAll();
+            alreadyTrigged = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            playerCount--;
+            print(playerCount);
+        }
+
+    }
 }
