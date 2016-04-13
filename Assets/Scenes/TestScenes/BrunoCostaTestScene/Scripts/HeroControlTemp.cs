3f62a97661796aa40ef01616a63301324a287bdc
brunoracosta <brunoracosta@yahoo.com.br>
2016-04-13 06:43:10 -0300

    Cena Teste Bruno Costa
    * Primeiro exemplo de elemento carregável
    
    * Versão da Cena com dois elementos carregáveis

---
+++ Assets/Scenes/TestScenes/BrunoCostaTestScene/Scripts/HeroControlTemp.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class HeroControlTemp : MonoBehaviour {
+	
+	private static float MOVE_THRESHOLD = 0.19f;
+	
+	private HeroTemp hero;
+	
+	// Input States
+	private float _moveButtonSpeed = 0.0f;
+	private bool _jumpButtonPressed = false;
+	private bool _crouchButtonPressed = false;
+	private bool _carryButtonPressed = false;
+	private bool _actionButtonPressed = false;
+	private bool _changeHeroButtonPressed = false;
+	
+	
+	void Awake () {
+		hero = GetComponent<HeroTemp> ();
+	}
+	
+	private void detectButtonStates(){
+		//Analog Input:
+		// Walk
+		_moveButtonSpeed = Input.GetAxis("Horizontal");
+		
+		//-------------------------------------------
+		
+		//Boolean Buttons, or teo state buttons:
+		// Crouch
+		if (Input.GetButton("Crouch"))
+			_crouchButtonPressed= true;
+		else
+			_crouchButtonPressed = false;
+		
+		// Push
+		if (Input.GetButton("Carry"))
+			_carryButtonPressed= true;
+		else
+			_carryButtonPressed = false;
+		
+		//----------------------------------------
+		
+		//Trigger Buttons, one time activation: (they are deactivated on Fixed Update)
+		
+		// Change Hero
+		if (Input.GetButtonDown("ChangeHero"))
+			_changeHeroButtonPressed = true;
+		
+		// Jump
+		if (Input.GetButtonDown("Jump"))
+			_jumpButtonPressed= true;
+		
+		// Action
+		if (Input.GetButtonDown("Action"))
+			_actionButtonPressed= true;
+	}
+	
+	void Update() {
+		detectButtonStates();
+	}
+	
+	void FixedUpdate () {
+		
+		if (!hero.IsActive) {
+			hero.StopWalk();
+			if (_changeHeroButtonPressed) {
+				hero.ChangeHero();
+			}
+			DeactivateTriggerButtons();
+			return;
+		}
+		
+		if (_crouchButtonPressed) {
+			hero.Crouch ();
+		} else {
+			hero.StandUp();
+		}
+		
+		if (_carryButtonPressed) {
+			hero.Carry ();
+		} else {
+			hero.StopCarry();
+		}
+
+		if (_changeHeroButtonPressed) {
+			hero.ChangeHero();
+			SoundManager.Instance.SendMessage("PlaySFXSwap");
+		}
+		
+		if (_jumpButtonPressed) {
+			hero.Jump ();
+		}
+		
+		if (_actionButtonPressed) {
+			hero.Action();
+		}
+		
+		if (Mathf.Abs (_moveButtonSpeed) > HeroControlTemp.MOVE_THRESHOLD) {
+			hero.Move (_moveButtonSpeed);
+		} else {
+			hero.Move (0.0f);
+		}
+		
+		DeactivateTriggerButtons();
+	}
+	
+	private void DeactivateTriggerButtons(){
+		
+		_changeHeroButtonPressed = false;
+		_jumpButtonPressed = false;
+		_actionButtonPressed = false;
+	}
+
+}
---
+++ Assets/Scenes/TestScenes/BrunoCostaTestScene/Scripts/HeroFastTemp.cs
@@ -0,0 +1,8 @@
+
+using System;
+public class HeroFastTemp : HeroTemp
+{
+	/*protected override void Awake(){
+		CalculateHeroHeight();
+	}*/
+}
