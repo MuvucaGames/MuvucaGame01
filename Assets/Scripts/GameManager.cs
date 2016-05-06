e21a07ec30e8e1e137e4073c9a34fe0f39d1a408
StefanWerW <scaw.dev@gmail.com>
2016-05-06 20:06:04 +0200

    Add functions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,30 +5,29 @@ public class GameManager : MonoBehaviour {
 	
 	public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
 
-	//Awake is always called before any Start functions
 	void Awake()
 	{
 		//Check if instance already exists
 		if (instance == null)
-
 			//if not, set instance to this
 			instance = this;
-
 		//If instance already exists and it's not this:
 		else if (instance != this)
-
 			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
 			Destroy(gameObject);    
-
 		//Sets this to not be destroyed when reloading scene
 		DontDestroyOnLoad(gameObject);
-
-		//Call the InitGame function to initialize the first level 
-		InitGame();
 	}
 
-	//Initializes the game for each level.
-	void InitGame(){
+	public void ReturnToMenu(){
+
+	}
+
+	public void Load(){
+
+	}
+
+	public void LoadNext(){
 
 	}
 }
