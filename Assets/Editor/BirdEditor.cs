a220c7602462aa470207ece4cc734f9a96ec10dc
Thiago Patrocinio <thiago9333@hotmail.com>
2016-05-14 15:00:13 -0300

    Issue #196
    Utilizar FixedUpdate ao invés de Update.
    Utilizar Coroutine.
    Habilitar edição visual do ponto B.

---
+++ Assets/Editor/BirdEditor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(MoveAtoB))]
+public class BirdEditor : Editor
+{
+    SerializedProperty pointA_SP; 
+    SerializedProperty pointB_SP;
+    SerializedProperty speed_SP;
+
+    public void OnEnable()
+    {
+        pointA_SP = serializedObject.FindProperty("pointA"); 
+        pointB_SP = serializedObject.FindProperty("pointB");
+        speed_SP = serializedObject.FindProperty("speed");
+    }
+    public void OnSceneGUI()
+    {
+        serializedObject.Update();
+        MoveAtoB platform = (MoveAtoB)target;   
+        Vector3 pointA = platform.transform.TransformPoint(new Vector3(pointA_SP.vector3Value.x, 0, 0));     
+
+        Handles.color = Color.blue;       
+   
+        Vector3 pointB = platform.transform.TransformPoint(new Vector3(pointB_SP.vector3Value.x, 0, 0));
+
+        Handles.color = Color.blue;
+        pointB = Handles.FreeMoveHandle(
+            pointB,
+            Quaternion.identity,
+            HandleUtility.GetHandleSize(pointB) * 0.08f,
+            Vector3.one * 0.1f,
+            Handles.DotCap);
+        
+        pointB_SP.vector3Value = platform.transform.InverseTransformPoint(pointB);
+        Handles.DrawDottedLine(pointA, pointB, 20f);
+
+        float speed = speed_SP.floatValue;   
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+
--- Assets/Scripts/Elements/MoveAtoB.cs
+++ Assets/Scripts/Elements/MoveAtoB.cs
@@ -1,22 +1,33 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using System.Collections;
 
-public class MoveAtoB : MonoBehaviour {
-
-	private Vector3 pointA;
-	[SerializeField]private Vector3 pointB = new Vector3(2,0,0);
-
-	[Range(0.0f, 20.0f)]public float speed = 2f;
-
-	void Awake () {
-		pointA = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-		pointB = transform.TransformPoint(pointB);
-	}
-
-	void Update() {
-		// based on the speed set movement step
-		float step = speed * Time.deltaTime;
-		// move GameObject from current position (GameObject own position or if defined point A position) to point B 
-		transform.position = Vector3.MoveTowards(transform.position, pointB, step);
-	}
+public class MoveAtoB : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 pointA;
+    [SerializeField]
+    private Vector3 pointB = new Vector3(2, 0, 0);    
+    [Range(0.0f, 20.0f)]
+    public float speed = 2f;    
+    void Awake()
+    {
+        pointA = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        pointB = transform.TransformPoint(pointB);
+    }
+    void FixedUpdate()
+    {
+        StartCoroutine(Move());
+    }
+    IEnumerator Move()
+    {
+        for (float f = 1f; f >= 0; f -= 0.1f)
+        {
+            // based on the speed set movement step
+            float step = speed * Time.deltaTime;
+            // move GameObject from current position (GameObject own position or if defined point A position) to point B 
+            transform.position = Vector3.MoveTowards(transform.position, pointB, step);
+            yield return null;
+        }
+    }              
 }
