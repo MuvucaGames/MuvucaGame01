f784205672dd47d66c7d86c128ebb33949908b91
Thiago Patrocinio <thiago9333@hotmail.com>
2016-05-15 14:57:52 -0300

    Remoção da variável moveCoroutine
    Remoção da variável moveCoroutine

--- Assets/Scripts/Elements/MoveAtoB.cs
+++ Assets/Scripts/Elements/MoveAtoB.cs
@@ -9,14 +9,12 @@ public class MoveAtoB : MonoBehaviour
     [SerializeField]
     private Vector3 pointB = new Vector3(2, 0, 0);
     [Range(0.0f, 20.0f)]
-    public float speed = 2f;
-	private Coroutine moveCoroutine;
+    public float speed = 2f;	
     void Awake()
     {
         pointA = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         pointB = transform.TransformPoint(pointB);
-		moveCoroutine = StartCoroutine(Move());
-
+		StartCoroutine(Move());
     }
     
     private IEnumerator Move()
@@ -26,6 +24,8 @@ public class MoveAtoB : MonoBehaviour
 			transform.position = Vector3.MoveTowards(transform.position, pointB, step);
 			yield return null;
 		}
+
+        yield return new WaitForSeconds(3f);
     }
 }
 
