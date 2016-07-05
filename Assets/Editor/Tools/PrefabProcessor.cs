87fc883080d882f1e9e8b97652b50e2dc60ae76a
StefanWerW <scaw.dev@gmail.com>
2016-07-05 21:20:52 +0200

    Prefab Processor Init

---
+++ Assets/Editor/Tools/PrefabProcessor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+/// <summary>
+/// Prefab processor.
+/// Verify that the prefab is created within our norms
+/// </summary>
+public class PrefabProcessor : UnityEditor.AssetModificationProcessor {
+
+	static string[] OnWillSaveAssets (string[] paths) {
+		Debug.Log("OnWillSaveAssets");
+		foreach(string path in paths)
+			Debug.Log(path);
+		return paths;
+	}
+
+	static string OnWillCreateAsset (string path) {
+		Debug.Log("OnWillCreateAssets");
+		Debug.Log(path);
+		return path;
+	}
+}
